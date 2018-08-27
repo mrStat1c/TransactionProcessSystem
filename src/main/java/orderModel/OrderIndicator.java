d1bcda6d068f56fb76c124a815b54f7b6811936b
mr_Static <amelin.ilya@gmail.com>
2018-08-27 22:13:23 +0300

    fixes

---
+++ src/main/java/orderModel/OrderIndicator.java
@@ -0,0 +1,10 @@
+package orderModel;
+
+/**
+ * Индикаторы, которые могут быть добавлены в заказ
+ */
+public enum OrderIndicator {
+    ALCOHOL,
+    LOTTERY,
+    PROD_LINE
+}
---
+++ src/main/java/orderModel/OrderPosition.java
@@ -0,0 +1,42 @@
+package orderModel;
+
+/**
+ * Класс, инкапсулирующий позицию в заказе
+ */
+public class OrderPosition {
+
+    private String product;
+    private String price;
+    private String count;
+    private boolean newProductInd;
+    private int number;
+
+    public OrderPosition(String product, String price, String count, boolean newProductInd, int number){
+        this.product = product;
+        this.price = price;
+        this.count = count;
+        this.newProductInd = newProductInd;
+        this.number = number;
+    }
+
+    public String getProduct(){
+        return this.product;
+    }
+
+    public String getPrice(){
+        return this.price;
+    }
+
+    public String getCount(){
+        return this.count;
+    }
+
+    public boolean getNewProductInd() {
+        return this.newProductInd;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
+
